7cad3171e23aba865a0c446286b4dded04117484
Istvan Mosonyi <imosonyi@LK7CH4YLJX.local>
2023-07-12 14:19:45 +0200

    Updated auth navigation header

--- src/feat/auth/auth.navigator.tsx
+++ src/feat/auth/auth.navigator.tsx
@@ -3,10 +3,19 @@ import {createStackNavigator} from "@react-navigation/stack";
 import Welcome from "./Welcome";
 import Login from "./Login";
 import Registration from "./Registration";
+import {Appbar} from "react-native-paper";
+import {NavigationProp} from "@react-navigation/native";
 
 const AuthStack = createStackNavigator();
+const AuthNavigatorHeader: (props: { navigation: NavigationProp<any>, back: boolean }) => JSX.Element =
+    ({navigation, back}) => (
+        <Appbar.Header statusBarHeight={0}>
+          {back && <Appbar.BackAction onPress={navigation.goBack}/>}
+        </Appbar.Header>
+    );
 
 const AuthNavigator: () => JSX.Element = () => (
+    // @ts-ignore
     <AuthStack.Navigator screenOptions={screenOptions}>
       <AuthStack.Screen name="Welcome" component={Welcome}/>
       <AuthStack.Screen name="Login" component={Login}/>
@@ -17,7 +26,5 @@ const AuthNavigator: () => JSX.Element = () => (
 export default AuthNavigator;
 
 const screenOptions = {
-  headerTransparent: true,
-  headerTitle: "",
-  headerStatusBarHeight: 0
+  header: AuthNavigatorHeader
 };
