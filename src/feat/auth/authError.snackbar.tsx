32a4ddf7ec58525f8982c986a0c3c61de9894924
Istvan Mosonyi <imosonyi@LK7CH4YLJX.local>
2023-07-12 13:18:18 +0200

    Auth error snackbar color fix

--- src/feat/auth/authError.snackbar.tsx
+++ src/feat/auth/authError.snackbar.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import {Snackbar, withTheme} from "react-native-paper";
+import {MD3Theme, Snackbar, withTheme} from "react-native-paper";
 import {StyleSheet, Text} from "react-native";
 
-const AuthErrorSnackbar: (props: { error: string, onDismiss: () => void }) => JSX.Element = ({error, onDismiss}) => (
-    <Snackbar visible={!!error}
-              onDismiss={onDismiss}
-              duration={3000}
-              wrapperStyle={styles.wrapper}
-              style={styles.snackbar}>
-      <Text style={styles.message}>{error}</Text>
-    </Snackbar>
-);
+const AuthErrorSnackbar: (props: { error: string, onDismiss: () => void, theme: MD3Theme }) => JSX.Element =
+    ({error, onDismiss, theme}) => (
+        <Snackbar visible={!!error}
+                  onDismiss={onDismiss}
+                  duration={3000}
+                  wrapperStyle={styles.wrapper}
+                  style={styles.snackbar}>
+          {/* @ts-ignore */}
+          <Text style={styles.message(theme.colors.surface)}>{error}</Text>
+        </Snackbar>
+    );
 
-export default AuthErrorSnackbar;
+export default withTheme(AuthErrorSnackbar);
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -21,7 +23,9 @@ const styles = StyleSheet.create({
   snackbar: {
     marginHorizontal: 100
   },
-  message: {
+  // @ts-ignore
+  message: (color) => ({
+    color: color,
     textAlign: "center"
-  }
+  })
 });
